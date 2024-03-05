7f2bf08fae6c58135c3a204af75a6935572e9ec5
William James <Williamj3795@gmail.com>
2024-03-05 15:51:41 -0500

    transform method created to filter trhought search bar

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -4,8 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-
+  
   transform(sourceList: {name:string, description:string, imagePath: string, 
     id: number }[], 
-    searchText:string){} 
-  }
+    searchText:string)
+    {
+      if (searchText === undefined || searchText.length == 0) { 
+        //No search term 
+        return sourceList 
+        } 
+        let reg = new RegExp(searchText, 'i') 
+        return sourceList.filter(courses => 
+        courses.name.search(reg) >= 0 || 
+        courses.description.search(reg) >= 0) 
+        }
+}
