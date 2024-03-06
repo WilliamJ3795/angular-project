f95bf2997c85a393eca9196768c713f2987ccef3
William James <Williamj3795@gmail.com>
2024-03-06 10:32:31 -0500

    added ReactiveFormModule to import and the imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
@@ -29,8 +29,8 @@ import { OrderByPipe } from './order-by.pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
-    
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
