c473ec1a334e174ca7d15aa9b95ae94512c1cffe
William James <Williamj3795@gmail.com>
2024-03-06 11:30:34 -0500

    added html for username login page, validation for signup form - contact ts page, ContactComponent has forbidden Emails method

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,13 +7,34 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  signupForm!: FormGroup; 
   ngOnInit() {
     this.signupForm = new FormGroup({ 
       'userData': new FormGroup({ 
-      }), 
-    }
-    );  
+        'username': new FormControl(null, [Validators.required]),
+        'email': new FormControl(null, [Validators.required, Validators.email,this.forbiddenEmails as ValidatorFn])
+        }), 
+
+      'courses': new FormArray([])
+    });  
+    
   }
-    signupForm!: FormGroup; 
+   onSubmit() { 
+    console.log(this.signupForm); 
+    this.signupForm.reset(); 
+    }
+    
+    forbiddenEmails(control: FormControl) { 
+      let email = control.value; 
+      if (email && email.indexOf("@") != -1) { 
+      let [_, domain] = email.split("@"); 
+      if (domain == "test.com") { 
+      return {'emailIsForbidden': true}; 
+      } 
+      return null; 
+      } 
+      return null; 
+      }
+
 
 }
