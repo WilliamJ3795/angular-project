c1051379f843eda7322be502284039ded6628413
William James <Williamj3795@gmail.com>
2024-03-06 12:27:21 -0500

    set routes for root, contact and courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,15 @@ import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component'; 
 import { CourseStartComponent } from './courses/course-start/course-start.component'; 
 import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/courses', pathMatch: 'full' }, 
+  { path: 'contact', component: ContactComponent, pathMatch: 'full' }, 
+  { path: 'courses', component: CoursesComponent, children: [ 
+    
+  ] }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
