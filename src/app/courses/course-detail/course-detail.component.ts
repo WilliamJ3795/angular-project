88e4a6ad47cbd13dcfbf31ceb2a846a8bdfe9cb1
William James <Williamj3795@gmail.com>
2024-03-11 11:45:03 -0400

    added CourseService import

--- src/app/courses/course-detail/course-detail.component.ts
+++ src/app/courses/course-detail/course-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Course } from '../course.model'; 
+import { CourseService } from '../course.service';
 
 @Component({
   selector: 'app-course-detail',
@@ -8,46 +9,22 @@ import { Course } from '../course.model';
   styleUrls: ['./course-detail.component.css']
 })
 export class CourseDetailComponent {
-  course!: Course;
-  id! : number;
   
-  getCourse(id: number) { 
-    return this.courses[id]; 
-    }
-    
-    ngOnInit() { 
-      this.route.params 
-      .subscribe( 
-      (params: Params) => { 
-      this.id = +params['id']; 
-      console.log(this.id) 
-      this.course = this.getCourse(this.id); 
-      } 
-      ); 
-      }
-
-  constructor(private router: Router,
-    private route: ActivatedRoute) { 
-    }
-
-    courses: Course[] = [ 
-      new Course('JavaScript', 
-      'Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web. JavaScript enables interactive web pages and is an essential part of web applications.', '/assets/javascript.jpg', 0), 
-      new Course('Angular', 
-      'A TypeScript-based open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations', 
-      '/assets/angular.jpg', 1), 
-      new Course('HTML', 
-      'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.', 
-      '/assets/html.jpg',2), 
-      new Course('React', 
-      `React can be used as a base in the development of single-page or 
-      mobile applications, as it's optimal only for its intended use of being 
-      the quickest method to fetch rapidly changing data that needs to be 
-      recorded. However, fetching data is only the beginning of what happens on 
-      a web page, which is why complex React applications usually require the 
-      use of additional libraries for state management, routing, and interaction 
-      with an API.`, 
-      '/assets/react.jpg',3), 
-      new Course('Vue.js', 
-      'Vue.js features an incrementally adoptable architecture that  on declarative rendering and component composition. Advanced features required for complex applications such as routing, state management and build tooling are offered via officially maintained supporting libraries and packages.', '/assets/vue.jpg',4) ];
+  course!: Course; 
+  id!: number; 
+  constructor(private courseService: CourseService, 
+  private route: ActivatedRoute, 
+  private router: Router) { 
+  } 
+  ngOnInit() { 
+  this.route.params 
+  .subscribe( 
+  (params: Params) => { 
+  this.id = +params['id']; 
+  console.log(this.id) 
+  this.course = this.courseService.getCourse(this.id); 
+  } 
+  ); 
+  }
+   
 }
